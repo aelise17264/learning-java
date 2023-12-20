7b8db1e06c588230623f07f68b318038b08789d9
Anna <aelise17264@gmail.com>
2023-12-20 12:00:39 -0700

    runtime polymorphism

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -1,24 +1,43 @@
 package OOP;
 import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
+
+    public static void addRandomNumber(ArrayList<Integer> list){
+        int originalSize = list.size();
+        Random random = new Random();
+        while(originalSize + 1 != list.size()){
+            int n = random.nextInt(1000);
+            list.add(n);
+        }
+    }
     public static void main(String[] args){
 
-        ModArrayList listy = new ModArrayList();
-        listy.add(0);        
-        listy.add(10);
-        listy.add(20);
-        listy.add(30);
-        listy.add(40);
-        listy.add(50);
+        OddArrayList oddListy = new OddArrayList(1, 2, 3, 4, 7, 9, -13);
+        addRandomNumber(oddListy);
+        System.out.println(oddListy);
+        System.out.println(oddListy.size());
 
-        System.out.println(listy.getUsingMod(1));        
-        System.out.println(listy.getUsingMod(-2));
-        System.out.println(listy.getUsingMod(50));
+        ArrayList<Integer> listy = new ArrayList<>();
+        listy.add(1);        
+        listy.add(2);
+        listy.add(3);
+        listy.add(4);
+        listy.add(7);
+        listy.add(9);        
+        listy.add(-13);
 
+        addRandomNumber(listy);
+        System.out.println(listy);
+        System.out.println(listy.size());
 
+       
+    }
+}
 
-        // Stack<Character> charStack = new Stack<>();
+ // Stack<Character> charStack = new Stack<>();
 
         // charStack.push('C');        
         // charStack.push('A');
@@ -62,6 +81,3 @@ public class Main {
 
         // System.out.println(account2.getOwner() + " has " + account2.getBalance() + " in their bank account");
         // account2.deposit();
-
-    }
-}
